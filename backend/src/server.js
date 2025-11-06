52d9049c5ef72df6c80fc21d596b23e41eae2c3a
sapeksh <sapekshvishwakarma@gmail.com>
2025-11-06 11:35:08 +0530

    add: implement server setup with dotenv configuration

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
