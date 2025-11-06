854acb8f5dd22a14a125b6cbe1d7789a2c3511ac
sparsh-15 <sparshsahu8435@gmail.com>
2025-11-06 16:29:08 +0530

    added auth middleware

---
+++ backend/src/middleware/authMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (!token) return res.status(401).json({ success: false, message: 'No token provided' });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch {
+    res.status(401).json({ success: false, message: 'Invalid token' });
+  }
+};
