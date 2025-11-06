c65a8ed4b75fc04d9745d31c06665114192e1262
sapeksh <sapekshvishwakarma@gmail.com>
2025-11-06 11:35:22 +0530

    DB COnnection

---
+++ backend/src/config/db.js
@@ -0,0 +1,9 @@
+// src/config/db.js
+import { PrismaClient } from '../generated/prisma';
+const prisma = new PrismaClient();
+
+prisma.$connect()
+  .then(() => console.log('✅ Connected to PostgreSQL via Prisma'))
+  .catch(err => console.error('❌ Prisma connection error:', err));
+
+export default prisma;
